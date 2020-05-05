eca7783fedd1b991531ef5891eaea13286355bd3
Ehab El-Sherif <ehabraelsherif@gmail.com>
2020-05-05 09:43:40 +0200

    variable password length, calculate HMAC and append it, use plaintext rather than json in encrypted file

--- decrypt.py
+++ decrypt.py
@@ -2,19 +2,27 @@ from Crypto.Cipher import DES
 from Crypto import Random
 from Crypto.Util import Counter
 from base64 import b64decode,b64encode
-import json
+from hashlib import pbkdf2_hmac
 
-inputFile = "test1.enc"
-outputFile = "test1.out"
-inputPassword = b'12345678'
+encFile = "test1.enc"
+decFile = "test1.out"
+inputPassword = '12345678'
 
-inputFile = open(inputFile)
-outputFile = open(outputFile,mode="w")
-jsonData = json.loads(inputFile.readline())
+encFile = open(encFile)
+decFile = open(decFile,mode="w")
+encData = encFile.read()
+nonce = encData[0:8]
+ct = encData[8:-28]
+hmac = encData[len(nonce)+len(ct):]
 
-key = inputPassword
-nonce = b64decode(jsonData['nonce'])
-ct = b64decode(jsonData['ciphertext'])
+print("Received:",encData)
+print("Nonce:",nonce)
+print("Ciphertext:",ct)
+print("HMAC:",hmac)
+
+key = pbkdf2_hmac('sha256',inputPassword.encode(),''.encode(),10000,8)
+nonce = b64decode(nonce)
+ct = b64decode(ct)
 
 # # 32-bit counter
 ctr = Counter.new(DES.block_size*4, prefix=nonce)
@@ -23,5 +31,5 @@ cipher = DES.new(key, DES.MODE_CTR, counter=ctr)
 data = cipher.decrypt(ct).decode()
 
 print(("%s") % (data))
-outputFile.write(data)
-outputFile.close()
+decFile.write(data)
+decFile.close()
--- encrypt.py
+++ encrypt.py
@@ -1,19 +1,40 @@
 from Crypto.Cipher import DES
 from Crypto import Random
 from Crypto.Util import Counter
+from hashlib import pbkdf2_hmac,sha1
 from base64 import b64encode
-import json
+
+def xor(x, y):
+    return bytes(x[i] ^ y[i] for i in range(min(len(x), len(y))))
+
+def hmac_sha1(key_K, data):
+    if len(key_K) > 64:
+        raise ValueError('The key must be <= 64 bytes in length')
+    padded_K = key_K + b'\x00' * (64 - len(key_K))
+    ipad = b'\x36' * 64
+    opad = b'\x5c' * 64
+    h_inner = sha1(xor(padded_K, ipad))
+    h_inner.update(data)
+    h_outer = sha1(xor(padded_K, opad))
+    h_outer.update(h_inner.digest())
+    return h_outer.digest()
+
+def do_tests(ct):
+    k = b'\x0b' * 20
+    result = hmac_sha1(k, ct)
+    return result
 
 inputFile = "test1.txt"
 outputFile = "test1.enc"
-inputPassword = b'12345678'
+inputPassword = '12345678'
 
 inputFile = open(inputFile)
 outputFile = open(outputFile,mode="w")
 lines = inputFile.readlines()
 data = ''.join(lines).encode()
-key = inputPassword
 
+# b64encode(hashlib.sha256(salt.encode() + password.encode()).digest())
+key = pbkdf2_hmac('sha256',inputPassword.encode(),''.encode(),10000,8)
 # 4-byte nonce
 nonce = Random.new().read(int(DES.block_size/2))
 # 32-bit counter
@@ -24,7 +45,13 @@ ct = cipher.encrypt(data)
 
 nonce = b64encode(cipher.nonce).decode('utf-8')
 ct = b64encode(ct).decode('utf-8')
-msg = json.dumps({'nonce':nonce, 'ciphertext':ct})
+hmac = b64encode(do_tests(ct.encode())).decode('utf-8')
+
+print("Nonce:",nonce)
+print("Ciphertext:",ct)
+print("HMAC:",hmac)
+
+msg = nonce + ct + hmac
 
 print(("Ciphertext:%s") % (msg))
 outputFile.write(msg)
